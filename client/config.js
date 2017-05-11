37bb03b8fa85c34d62a989a324be717e4a08adb7
weihanchen <willhanchen@gmail.com>
2017-05-11 23:08:25 +0800

    style(client): index

---
+++ client/config.js
@@ -0,0 +1,6 @@
+export const ENDPOINT = process.env.NODE_ENV === 'production' ? `${window.location.origin}` : 'http://localhost:3000';
+export const API_ENDPOINT = `${ENDPOINT}/api`;
+export const GRAPHQL_ENDPOINT = `${ENDPOINT}/graphql`;
+export const LOGIN_URL = `${API_ENDPOINT}/users/login`;
+export const LOGOUT_URL = `${API_ENDPOINT}/users/logout`;
+export const USERS_URL = `${API_ENDPOINT}/users`;
--- client/index.js
+++ client/index.js
@@ -7,3 +7,12 @@ import {
 	browserHistory,
 	hashHistory
 } from 'react-router';
+
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { GRAPHQL_ENDPOINT } from './config'
+const networkInterface = createNetworkInterface({ uri: GRAPHQL_ENDPOINT });
+
+const client = new ApolloClient({
+  networkInterface
+});
