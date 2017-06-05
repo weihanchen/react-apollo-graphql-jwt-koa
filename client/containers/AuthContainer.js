86d65d8eef3ed276ff6b9648a68dffe03f3b7a1a
weihanchen <willhanchen@gmail.com>
2017-06-05 23:23:26 +0800

    refactor(client): AuthContainer use async

--- client/containers/AuthContainer.js
+++ client/containers/AuthContainer.js
@@ -1,81 +1,82 @@
 import React, {
-	Component,
+   Component,
 } from 'react';
 import PropTypes from 'prop-types';
 import {
-	connect
+   connect
 } from 'react-redux';
 import {
-	bindActionCreators
+   bindActionCreators
 } from 'redux';
 import { graphql, withApollo } from 'react-apollo';
 import ApolloClient from 'apollo-client';
 
 import LinearProgress from 'material-ui/LinearProgress';
 import {
-	requestAuthentication,
-	requestAuthenticationSuccess,
-	requestFaild
+   requestAuthentication,
+   requestAuthenticationSuccess,
+   requestFaild
 } from '../actions';
 import getCurrentUser from '../graphql/CurrentUserQuery.graphql';
 
 class AuthContainer extends Component {
 
-	componentDidMount() {
-		const token = localStorage.getItem('token');
-		const { history } = this.props;
-		if (!token) history.push('/login')
-		else {
-			this.props.requestAuthentication();
-			this.props.client.query({
-				query: getCurrentUser
-			})
-			.then(result => this.props.requestAuthenticationSuccess(result.data.Me))
-			.catch(error => {
-                this.props.requestFaild(error)
+   async componentDidMount() {
+      try {
+         const token = localStorage.getItem('token');
+         const { history } = this.props;
+         if (!token) throw '';
+         else {
+            this.props.requestAuthentication();
+            const currentUser = await this.props.client.query({
+               query: getCurrentUser
             });
-		}
-	}
+            this.props.requestAuthenticationSuccess(result.data.Me);
+         }
+      } catch (error) {
+          this.props.requestFaild(error);
+      }
+   }
 
-	componentWillReceiveProps(nextProps) {
-		const nextStatus = nextProps.authentication.status
-		const { authentication, history } = this.props;
-		const currentStatus = authentication.status;
-		const statusFunction = {
-			'success': function() {
-				history.replace('/profile')
-			},
-			'error': function() {
-				history.replace('/login')
-			}
-		}
-		if (currentStatus != nextStatus && statusFunction.hasOwnProperty(nextStatus)) statusFunction[nextStatus]()
-	}
+   componentWillReceiveProps(nextProps) {
+      const nextStatus = nextProps.authentication.status
+      const { authentication, history } = this.props;
+      const currentStatus = authentication.status;
+      const statusFunction = {
+         'success': function () {
+            history.replace('/profile');
+         },
+         'error': function () {
+            history.replace('/login');
+         }
+      }
+      if (currentStatus != nextStatus && statusFunction.hasOwnProperty(nextStatus)) statusFunction[nextStatus]()
+   }
 
-	render() {
-		return (
-			<LinearProgress mode="indeterminate" />
-		)
-	}
+   render() {
+      return (
+         <LinearProgress mode="indeterminate" />
+      )
+   }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		authentication: state.authentication
-	}
+   return {
+      authentication: state.authentication
+   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-	return bindActionCreators({
-		requestAuthentication,
-		requestFaild,
-		requestAuthenticationSuccess
-	}, dispatch)
+   return bindActionCreators({
+      requestAuthentication,
+      requestFaild,
+      requestAuthenticationSuccess
+   }, dispatch)
 }
 
 AuthContainer.propTypes = {
-	requestAuthentication: PropTypes.func,
-	client: PropTypes.instanceOf(ApolloClient).isRequired
+   requestAuthentication: PropTypes.func,
+   client: PropTypes.instanceOf(ApolloClient).isRequired
 }
 
 const AuthWithApollo = withApollo(AuthContainer);
